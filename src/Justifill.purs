3713c7ba4497f42d662f2534e8e4d81c7f4e3ad5
Mark Eibes <mark.eibes@gmail.com>
2020-11-22 09:57:50 +0100

    Add justifillExplicit

--- src/Justifill.purs
+++ src/Justifill.purs
@@ -3,6 +3,7 @@ module Justifill where
 import Prelude
 import Justifill.Fillable (class Fillable, fill)
 import Justifill.Justifiable (class Justifiable, justify)
+import Type.Proxy (Proxy)
 
 --| Completes records by wrapping provided optional keys in `Just`
 --| and filling out non-provided keys with `Nothing`s
@@ -16,3 +17,14 @@ justifill ∷
   Justifiable { | from } { | thru } =>
   { | from } -> { | to }
 justifill = (fill ∷ { | thru } -> { | to }) <<< (justify ∷ { | from } -> { | thru })
+
+--| A version of justifill that helps type inference along
+--| by receiving a `Proxy` of the `thru` type
+--| This is useful if you write a wrapper around justifill because
+--| it allows to write simpler type annotations
+justifillExplicit ∷
+  ∀ from thru to.
+  Fillable { | thru } { | to } =>
+  Justifiable { | from } { | thru } =>
+  Proxy { | thru } -> { | from } -> { | to }
+justifillExplicit _ = (fill ∷ { | thru } -> { | to }) <<< (justify ∷ { | from } -> { | thru })
